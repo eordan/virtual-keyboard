26f6f3f13db9cc7f2337761d7814c0a1427a1061
eordan <pistsouandrey@gmail.com>
2023-04-29 00:59:37 +0300

    feat: implement basic functionality of virtual keys

--- app.js
+++ app.js
@@ -6,27 +6,27 @@ const EN_LOWERCASE = [
   ["Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "←", "↓", "→", "EN"]
 ];
 
-const EN_UPPERCASE = [
-  ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
-  ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", "Del"],
-  ["Caps Lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter"],
-  ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "↑", "Shift"],
+const EN_UPPERCASE = [ 
+  ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Backspace"],
+  ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|", "Del"],
+  ["Caps Lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "\"", "Enter"],
+  ["Shift", "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "↑", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "←", "↓", "→", "EN"]
 ];
 
-const RU_LOWERCASE = [
-  ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
-  ["Tab", "й", "ц", "у", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del"],
-  ["Caps Lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter"],
-  ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "↑", "Shift"],
+const RU_LOWERCASE = [ 
+  ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
+  ["Tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\", "Del"],
+  ["Caps Lock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "Enter"],
+  ["Shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "↑", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "←", "↓", "→", "RU"]
 ];
 
-const RU_UPPERCASE = [
-  ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
-  ["Tab", "Й", "Ц", "У", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", "Del"],
-  ["Caps Lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter"],
-  ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "↑", "Shift"],
+const RU_UPPERCASE = [ 
+  ["Ё", "!", "\"", "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "Backspace"],
+  ["Tab", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "/", "Del"],
+  ["Caps Lock", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Enter"],
+  ["Shift", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ",", "↑", "Shift"],
   ["Ctrl", "Win", "Alt", " ", "Alt", "Ctrl", "←", "↓", "→", "RU"]
 ];
 
@@ -43,9 +43,6 @@ if (localStorage.getItem('isEnglish') !== null) {
 
 generateKeybpardArea();
 generateKeyboard(isEnglish ? EN_LOWERCASE : RU_LOWERCASE);
-
-
-
 
 // Switch lowercase and upper case
 document.addEventListener("keydown", (event) => {
@@ -84,22 +81,47 @@ function generateRow(row) {
   generatedRow.classList.add("row");
 
   for (let i = 0; i < row.length; i++) {
+    const textArea = document.querySelector('.area-container');
     const key = document.createElement("div");
     key.classList.add("keys");
+    key.textContent = row[i];
 
     // Add a custom class to certain keys to make them wider
     switch (row[i]) {
       case "Enter":
         key.classList.add("enter");
+        key.onclick = () => { textArea.value += '\n' };
         break;
       case "Shift":
         key.classList.add("shift");
+        key.addEventListener('mousedown', () => {
+          isShiftPressed = true;
+          generateKeyboard(isEnglish ? EN_UPPERCASE : RU_UPPERCASE);
+        });
+        key.addEventListener('mouseup', () => {
+          isShiftPressed = false;
+          generateKeyboard(isEnglish ? EN_LOWERCASE : RU_LOWERCASE);
+        });
         break;
       case "Backspace":
         key.classList.add("backspace");
+
+        key.addEventListener('click', () => {
+          const cursorPosition = textArea.selectionStart;
+          textArea.focus();
+          if (cursorPosition === textArea.value.length) {
+            textArea.value = textArea.value.substring(0, textArea.value.length - 1);
+          } else {
+            textArea.value = textArea.value.slice(0, cursorPosition - 1) + textArea.value.slice(cursorPosition);
+            textArea.selectionStart = cursorPosition - 1;
+            textArea.selectionEnd = cursorPosition - 1;
+          };
+        });
+
         break;
       case "Tab":
         key.classList.add("tab");
+        key.onclick = () => { textArea.value += '\t' };
         break;
       case "Caps Lock":
         key.classList.add("caps-lock");
@@ -115,10 +137,10 @@ function generateRow(row) {
         key.classList.add("lang-key");
         break;
       default:
-        key.classList.add("letter");
+        key.classList.add("symbol");
+        key.onclick = () => { textArea.value += key.textContent; };
     }
 
-    key.textContent = row[i];
     generatedRow.appendChild(key);
   }
 
@@ -148,22 +170,4 @@ function generateKeybpardArea() {
   areaContainer.classList.add('area-container');
   areaContainer.setAttribute('autofocus', '');
   document.body.appendChild(areaContainer);
-}
-
-function addKeyListeners() {
-  const keys = document.querySelectorAll('.key');
-
-  keys.forEach(key => {
-    key.addEventListener('mousedown', () => {
-      key.classList.add('active');
-    });
-
-    key.addEventListener('mouseup', () => {
-      key.classList.remove('active');
-    });
-
-    key.addEventListener('mouseleave', () => {
-      key.classList.remove('active');
-    });
-  });
 }
